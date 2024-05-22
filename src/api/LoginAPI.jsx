90b91077be2a0d578b79db2a91c21a0e0236f63d
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-05-22 18:07:09 +0200

    Fixed API endpoint

--- src/api/LoginAPI.jsx
+++ src/api/LoginAPI.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const LoginAPI = {
-    loginUser: (loginCreds) => axios.post("http://localhost:8080/player", loginCreds)
+    loginUser: (loginCreds) => axios.post("http://localhost:8080/tokens", loginCreds)
 }
 
 export default LoginAPI;
