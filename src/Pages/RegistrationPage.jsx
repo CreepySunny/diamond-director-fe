a05acfcdb2fa541fb14f98bda82a7ccab3db2c4d
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-07 12:47:07 +0200

    New page structure

--- src/Pages/RegistrationPage.jsx
+++ src/Pages/RegistrationPage.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import RegistrationForm from "../component/RegistratioForm";
+import NavigationBar from "../component/Navbar";
+import { Container } from "react-bootstrap";
 
 function RegistrationPage(){
     return (
         <>
+        <NavigationBar />
+        <Container>
         <h1>register</h1>
         <RegistrationForm/>
+        </Container>
         </>
     )
 }
