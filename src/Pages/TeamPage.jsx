f6210b7d21ca61f753fcab38cc0c1d40e1c13e5c
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-14 06:26:22 +0200

    TeamPage

---
+++ src/Pages/TeamPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import RegistrationForm from "../component/RegistratioForm";
+import NavigationBar from "../component/Navbar";
+import { Container } from "react-bootstrap";
+
+function RegistrationPage(){
+    return (
+        <>
+        <NavigationBar />
+        <Container>
+            
+        </Container>
+        </>
+    )
+}
+
+export default RegistrationPage;
