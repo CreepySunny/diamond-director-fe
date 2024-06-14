114a052411601b6f89cef4824b5e1735bd391059
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-14 06:25:00 +0200

    Find Coach from a team and with no team

--- src/api/CoachAPI.jsx
+++ src/api/CoachAPI.jsx
@@ -1,7 +1,37 @@
 import axios from "axios";
 
+const getAuthHeaders = (token) => ({
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+});
+
 const CoachAPI = {
-    createCoach: (newCoach) => axios.post("http://localhost:8080/coach", newCoach)
+    createCoach: (newCoach) => axios.post("http://localhost:8080/coach", newCoach),
+
+    findCoachesFromTeamName: (teamName, token) => {
+        return axios.get(`http://localhost:8080/coach/team/${teamName}`, getAuthHeaders(token))
+            .then(response => ({
+                exception: null,
+                coaches: response.data
+            }))
+            .catch(error => ({
+                exception: error,
+                coaches: []
+            }));
+    },
+
+    findCoachNoTeam: (token) => {
+        return axios.get("http://localhost:8080/coaches/no-team", getAuthHeaders(token))
+            .then(response => ({
+                exception: null,
+                coaches: response.data
+            }))
+            .catch(error => ({
+                exception: error,
+                coaches: []
+            }));
+    }
 }
 
 export default CoachAPI;
