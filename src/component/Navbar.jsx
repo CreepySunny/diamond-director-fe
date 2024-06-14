c0b1f79cc7a5540de394d613c26cba6642fd917a
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-14 12:11:29 +0200

    Fixed navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const NavigationBar = () => {
   return (
-    <Navbar bg="light" expand="lg" className='fixed-top'>
-      <Navbar.Brand href="/">Diamond Director</Navbar.Brand>
+    <Navbar bg="light" expand="lg">
+      <LinkContainer to="/">
+        <Navbar.Brand>Diamond Director</Navbar.Brand>
+      </LinkContainer>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/about-us">About Us</Nav.Link>
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+        <Nav>
+          <LinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/about-us">
+            <Nav.Link>About Us</Nav.Link>
+          </LinkContainer>
         </Nav>
-        <Form inline className='ml-auto'>
+        <Form inline className="ml-auto">
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-success">Search</Button>
         </Form>
-        <Nav className="ml-md-auto">
-          <Nav.Link href="/login">Login</Nav.Link>
-          <Nav.Link href="/register">Register</Nav.Link>
+        <Nav className="ml-auto">
+          <LinkContainer to="/login">
+            <Nav.Link>Login</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/register">
+            <Nav.Link>Register</Nav.Link>
+          </LinkContainer>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
