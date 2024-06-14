ab7b4a88d293aed377b13865cb0395af2007d5f3
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-14 06:26:34 +0200

    Add Coach List

--- src/Pages/TeamPage.jsx
+++ src/Pages/TeamPage.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import RegistrationForm from "../component/RegistratioForm";
 import NavigationBar from "../component/Navbar";
 import { Container } from "react-bootstrap";
+import TeamCoachList from "../component/TeamCoachList";
 
-function RegistrationPage(){
+function TeamPage(){
     return (
         <>
         <NavigationBar />
         <Container>
-            
+            <TeamCoachList />
         </Container>
         </>
     )
 }
 
-export default RegistrationPage;
+export default TeamPage;
