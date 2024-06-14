549e8413df5a19359aa8c0704f9bd14835e95012
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-14 12:12:14 +0200

    New team page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RegistrationPage from './Pages/RegistrationPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router , Route, Routes} from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import NoPage from './Pages/NoPage';
 import LoginPage from './Pages/LoginPage';
 import ScoreKeepersPage from './Pages/ScoreKeepersPage';
+import TeamPage from './Pages/TeamPage';
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='register' element={<RegistrationPage/>} />
-        <Route path='games' element={<ScoreKeepersPage/>} />
-        <Route path='login' element={<LoginPage/>} />
-        <Route path='*' element={<NoPage/>}/>
-    </Routes>
-    </BrowserRouter>
+    <Router>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='register' element={<RegistrationPage/>} />
+          <Route path='login' element={<LoginPage/>} />
+          <Route path='game' element={<ScoreKeepersPage />} />
+          <Route path='team' element={<TeamPage/>} />
+          <Route path='*' element={<NoPage/>}/>
+        </Routes>
+    </Router>
     </>
   )
 }
--- src/Pages/TeamPage.jsx
+++ src/Pages/TeamPage.jsx
@@ -1,18 +1,48 @@
-import React from "react";
-import RegistrationForm from "../component/RegistratioForm";
+import React, { useState, useEffect } from "react";
 import NavigationBar from "../component/Navbar";
 import { Container } from "react-bootstrap";
 import TeamCoachList from "../component/TeamCoachList";
+import CreateTeamForm from "../component/CreateTeamForm";
+import TeamAPI from "../api/TeamAPI";
+import { jwtDecode } from 'jwt-decode';
 
 function TeamPage(){
-    return (
-        <>
-        <NavigationBar />
-        <Container>
-            <TeamCoachList />
-        </Container>
-        </>
-    )
+  const user = jwtDecode(sessionStorage.getItem('token'));
+  const [team, setTeam] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUserTeam(user.sub, sessionStorage.getItem('token'));
+  }, []);
+
+  const fetchUserTeam = (userEmail, token) => {
+    TeamAPI.findTeamFromUserEmail(userEmail, token)
+      .then(response => {
+          setTeam(response.data);
+          setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching team:', error);
+        setLoading(false);
+      });
+  };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <NavigationBar />
+      <Container>
+        {team ? (
+          <TeamCoachList teamName={team.teamName} />
+        ) : (
+          <CreateTeamForm user={user} />
+        )}
+      </Container>
+    </>
+  );
 }
 
 export default TeamPage;
