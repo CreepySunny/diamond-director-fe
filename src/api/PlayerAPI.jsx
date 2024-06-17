4367d26f1271a7d19a0493bd9b66902a9d49748d
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-17 20:07:30 +0200

    Auth header, player no team, players from team name.

--- src/api/PlayerAPI.jsx
+++ src/api/PlayerAPI.jsx
@@ -1,7 +1,18 @@
 import axios from "axios";
 
+const getAuthHeaders = (token) => ({
+    headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+    }
+});
+
 const PlayerAPI = {
-    createPlayer: (newPlayer) => axios.post("http://localhost:8080/player", newPlayer)
+    createPlayer: (newPlayer) => axios.post("http://localhost:8080/player", newPlayer),
+
+    findPlayersFromTeamName: (teamName, token) => axios.get(`http://localhost:8080/player/team/${teamName}`, getAuthHeaders(token)),
+
+    findPlayersNoTeam: (token) => axios.get("http://localhost:8080/player/no-team", getAuthHeaders(token))
 }
 
 export default PlayerAPI;
