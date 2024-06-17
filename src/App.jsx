491ee1b45a0523e4f460f7dd1b74d31b7e89c887
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-17 20:06:38 +0200

    Route for live

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import { BrowserRouter as Router , Route, Routes} from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import NoPage from './Pages/NoPage';
 import LoginPage from './Pages/LoginPage';
-import ScoreKeepersPage from './Pages/ScoreKeepersPage';
+import GamePage from './Pages/GamePage';
 import TeamPage from './Pages/TeamPage';
+import GameScorePage from './Pages/GameScoresPage';
 
 function App() {
 
@@ -17,8 +18,9 @@ function App() {
           <Route path='/' element={<HomePage/>}/>
           <Route path='register' element={<RegistrationPage/>} />
           <Route path='login' element={<LoginPage/>} />
-          <Route path='game' element={<ScoreKeepersPage />} />
+          <Route path='game' element={<GamePage />} />
           <Route path='team' element={<TeamPage/>} />
+          <Route path='live' element={<GameScorePage />} />
           <Route path='*' element={<NoPage/>}/>
         </Routes>
     </Router>
