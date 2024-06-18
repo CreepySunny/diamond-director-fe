fa64155d57f213ab93e807c07d8974781866af36
Sidney Thiel <9028-I499982@users.noreply.git.fhict.nl>
2024-06-18 06:37:57 +0200

    Cleanup navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,34 +1,31 @@
-import React from 'react';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import React, { useContext } from 'react';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import AuthContext from '../Auth/AuthContext';
 
 const NavigationBar = () => {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <Navbar bg="light" expand="lg">
       <LinkContainer to="/">
         <Navbar.Brand>Diamond Director</Navbar.Brand>
       </LinkContainer>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
-        <Nav>
-          <LinkContainer to="/">
-            <Nav.Link>Home</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/about-us">
-            <Nav.Link>About Us</Nav.Link>
-          </LinkContainer>
-        </Nav>
-        <Form inline className="ml-auto">
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-        <Nav className="ml-auto">
-          <LinkContainer to="/login">
-            <Nav.Link>Login</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/register">
-            <Nav.Link>Register</Nav.Link>
-          </LinkContainer>
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav className="ml-2">
+          {user ? (
+            <Button variant="outline-primary" onClick={logout}>Logout</Button>
+          ) : (
+            <>
+              <LinkContainer to="/login">
+                <Button variant="outline-success">Login</Button>
+              </LinkContainer>
+              <LinkContainer to="/register">
+                <Button variant="success">Register</Button>
+              </LinkContainer>
+            </>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
